5b3ec73b65528cefdeef60681fd33e9a0ea2ba56
unknown <gopeer@NAC-003870.telkom.co.za>
2018-02-18 20:53:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+ 
+ 
+      function myFunc(x, y){
+
+        let array= []
+        
+        for (var i = 0 ; i<x ; i++){
+        
+                array.push(i);
+        }
+        
+        for (var l= 0 ; l < y ; l++){
+        
+            array.push(l);
+        }
+        
+        return array
+    }   
+        
+    function myFunc(x, y){
+        
+            let array= []
+           
+            for (var i = 0 ; i<x ; i++){
+        
+                    array.push(i);
+            }
+            
+            for (var l= 0 ; l < y ; l++){
+        
+                array.push(l);
+            }
+            
+            return array
+        
+            
+        }
